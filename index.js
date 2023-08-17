b02c9ca56a6ce2b3e3c009a131c737a3d3107802
AfroGit <17697323-AfroGit@users.noreply.replit.com>
2023-08-17 15:08:42 +0000

    Primitive data types are immutable and are not objects because they lack properties and methods.

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+To determine the kind of data you are working with, use the typeof operator. The typeof operator works perfectly with all primitive data types except null.
+*/ 
+
+
+// let num = 23;
+
+// console.log(typeof num)
